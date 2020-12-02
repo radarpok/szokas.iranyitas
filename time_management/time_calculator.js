f2c5dd1298ba44745d01412fd9d46c9bdc30b104
radarpok <vdiana.m8@gmail.com>
2020-12-02 15:02:37 +0100

    adding habit.builder for organized schedule

---
+++ time_management/time_calculator.js
@@ -0,0 +1,245 @@
+// konstansok
+const days_in_year  = 365;
+const hours_in_day  = 24;
+const hours_in_year = 8760;
+
+// osszeszurni jarvissal
+const borbely_tamas_ferenc = {
+    bevasarlas:()=>{}
+}
+
+// 
+const bevasarlas = {
+    ido:{
+        becsult: "20",
+        meresek: []
+    },
+    helyszin:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const munka = {
+    ido:{
+        becsult: "360",
+        meresek: []
+    },
+    helyszin:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const fozes = {
+    ido:{
+        becsult: "50",
+        meresek: []
+    },
+    recept:{},
+    hozzavalok:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const takaritas = {
+    ido:{
+        becsult: "20",
+        meresek: []
+    },
+    eszkozok:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const mosogatas = {
+    ido:{
+        becsult: "20",
+        meresek: []
+    },
+    eszkozok:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const furdes = {
+    ido:{
+        becsult: "20",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const fogmosas = { 
+    ido:{
+        becsult: "5",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const fogselymezes = {
+    ido:{
+        becsult: "5",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const utazas = {
+    ido:{
+        becsult: "60",
+        meresek: []
+    },
+    helyszin:{},
+    algoritmus: {}
+}
+const konyveles = {
+    ido:{
+        becsult: "20",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const alomtabla_megnezese = {
+    ido:{
+        becsult: "10",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const megerositesek = {
+    ido:{
+        becsult: "10",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const edzes = {
+    ido:{
+        becsult: "40",
+        meresek: []
+    },
+    eszkozok:{},
+    tipus:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const olvasas = {
+    ido:{
+        becsult: "30",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const memorizalas = {
+    ido:{
+        becsult: "30",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const tanulas = {
+    ido:{
+        becsult: "60",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const kuzdosport = {
+    ido:{
+        becsult: "40",
+        meresek: []
+    },
+    eszkozok:{},
+    tipus:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const kikapcsolodas = {
+    ido:{
+        becsult: "30",
+        meresek: []
+    },
+    eszkozok:{},
+    tipus:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const zeneles = {
+    // dj, zongora
+    ido:{
+        becsult: "20",
+        meresek: []
+    },
+    eszkozok:{},
+    mufaj:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const alvas = {
+    ido:{
+        becsult: "480",
+        meresek: []
+    },
+    fazisok:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const szauna = {
+    ido:{
+        becsult: "20",
+        meresek: []
+    },
+    helyszin:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const nyujtas = {
+    ido:{
+        becsult: "30",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const memoriapalotagyujtes = {
+    ido:{
+        becsult: "20",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const meetup = {
+    ido:{
+        becsult: "90",
+        meresek: []
+    },
+    helyszin:{},
+    tipus:{},
+    gyakorisag:{},
+    algoritmus: {}
+}
+const testsulymeres = {
+    ido:{
+        becsult: "2",
+        meresek: []
+    },
+    gyakorisag:{}
+}
+const rajzolas = {
+    ido:{
+        becsult: "30",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
+const interface_tervezes = {
+    ido:{
+        becsult: "60",
+        meresek: []
+    },
+    gyakorisag:{},
+    algoritmus: {}
+}
