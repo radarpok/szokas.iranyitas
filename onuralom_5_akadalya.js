851e5fb976525c91f6b388d31204a569deef414c
radarpok <vdiana.m8@gmail.com>
2021-04-27 15:23:58 +0200

    adding 5 hindrance of self-mastery based on buddhist monastic orders

---
+++ onuralom_5_akadalya.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-multi-str */
+module.exports.onuralom_5_akadalya=()=>{
+
+let okok = {
+        1: "kamacchanda - érzéki vágy",
+        2: "byapada - rosszindulat / ellenszenv",
+        3: "thinamiddha - unalom / levertség",
+        4: "uddhacca - nyugtalanság",
+        5: "vicikiccha - szkeptikus kétely"
+    }
+return console.log(okok)
+};
